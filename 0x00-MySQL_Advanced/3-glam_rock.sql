5d7bbfd500f4f0e412f06aa368cda35523e864ee
Bigizic <olalekanisaac75@gmail.com>
2023-12-15 18:15:06 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,4 +2,5 @@
 -- ranked by their longevity
 SELECT band_name, (2022 - formed) - (2022 - split) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
