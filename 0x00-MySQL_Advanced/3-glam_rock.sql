a97c3cc597fc58f3ef5b421e6cc27a576b7cd530
Bigizic <olalekanisaac75@gmail.com>
2023-12-15 18:12:19 +0000

    Create 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- a SQL script that lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, (2022 - formed) - (2022 - split) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
