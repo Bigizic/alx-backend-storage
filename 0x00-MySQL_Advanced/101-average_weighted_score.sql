742514c9aa9f203beba20772934d84992270919a
Bigizic <olalekanisaac75@gmail.com>
2023-12-15 22:48:50 +0000

    Update average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,47 +1,36 @@
 -- a SQL script that creates a stored procedure
 -- ComputeAverageWeightedScoreForUsers that computes and store
 -- the average weighted score for all students
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER ##
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-	DECLARE done INT DEFAULT FALSE;
-	DECLARE user_id_val INT;
-	DECLARE total_score DECIMAL(10, 2);
-	DECLARE total_weight DECIMAL(10, 2);
-	DECLARE average_score DECIMAL(10, 2);
+    ALTER TABLE users ADD total_weighted_score INT NOT NULL;
+    ALTER TABLE users ADD total_weight INT NOT NULL;
 
-	DECLARE users_cursor CURSOR FOR 
+    UPDATE users
+        SET total_weighted_score = (
+            SELECT SUM(corrections.score * projects.weight)
+            FROM corrections
+                INNER JOIN projects
+                    ON corrections.project_id = projects.id
+            WHERE corrections.user_id = users.id
+            );
 
-	SELECT DISTINCT user_id FROM scores;
+    UPDATE users
+        SET total_weight = (
+            SELECT SUM(projects.weight)
+                FROM corrections
+                    INNER JOIN projects
+                        ON corrections.project_id = projects.id
+                WHERE corrections.user_id = users.id
+            );
 
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-	OPEN users_cursor;
-	read_loop: LOOP
-	FETCH users_cursor INTO user_id_val;
-	IF done THEN
-		LEAVE read_loop;
-	END IF;
-
-	SELECT SUM(score * weight), SUM(weight)
-	INTO total_score, total_weight
-	FROM scores
-        WHERE user_id = user_id_val;
-
-        IF total_weight > 0 THEN
-            SET average_score = total_score / total_weight;
-        ELSE
-            SET average_score = 0;
-        END IF;
-
-        INSERT INTO average_weighted_scores (user_id, average_score)
-        VALUES (user_id_val, average_score)
-        ON DUPLICATE KEY UPDATE average_score = average_score;
-    END LOOP;
-
-    CLOSE users_cursor;
-    
-END $$
-
+    UPDATE users
+        SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
+    ALTER TABLE users
+        DROP COLUMN total_weighted_score;
+    ALTER TABLE users
+        DROP COLUMN total_weight;
+END ##
 DELIMITER ;
