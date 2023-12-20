da3a14268d63b2e1647f27b2c796c23cce18e10e
Bigizic <olalekanisaac75@gmail.com>
2023-12-20 19:35:49 +0000

    add web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
-"""an expiring web cache and tracker
+"""A module with tools for request caching and tracking.
 """
-
-from functools import wraps
 import redis
 import requests
+from functools import wraps
 from typing import Callable
 
 
-redis_history = redis.Redis()
-""" redis instance """
+redis_store = redis.Redis()
+"""The module-level Redis instance.
+"""
 
 
-def fetch_data(method: Callable) -> Callable:
-    """Fetches the output from cached data
+def data_cacher(method: Callable) -> Callable:
+    """Caches the output of fetched data.
     """
     @wraps(method)
-    def cacher(url) -> str:
-        """Caches the output
+    def invoker(url) -> str:
+        """The wrapper function for caching the output.
         """
-        redis_history.incr(f'count:{url}')
-        res = redis_history.get(f'result:{url}')
-        if res:
-            return res.decode('utf-8')
-        res = method(url)
-        redis_history.set(f'count:{url}', 0)
-        redis_history.setex(f'result:{url}', 10, res}
-        return res
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-    return cacher
 
-
-@fetch_data
+@data_cacher
 def get_page(url: str) -> str:
-    """Uses requests module to obtain the HTML content of a particular URL
-    and returns it
-    @param (url): <str>
-
-    Return: <str>
+    """Returns the content of a URL after caching the request's response,
+    and tracking the request.
     """
     return requests.get(url).text
